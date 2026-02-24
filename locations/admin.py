c1ceaa5c29b5a1f8b048caa52d75a8e892bda7ca
Uwasa Ryohma <r.uwasa1130@gmail.com>
2026-02-24 16:03:35 +0900

    feat(locations): add Device/Location models and migrations

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Device, Location
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ("id", "device_id", "created_at")
+    search_fields = ("device_id",)
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ("id", "device", "latitude", "longitude", "accuracy", "recorded_at")
+    list_filter = ("recorded_at",)
--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Device(models.Model):
+    device_id = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.device_id
+
+
+class Location(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE, related_name="locations")
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    accuracy = models.FloatField(null=True, blank=True)
+    recorded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.device.device_id} @ {self.latitude}, {self.longitude}"
